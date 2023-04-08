c76461fd6a9f5a0a49e0ac304263d4f10367d9fd
HaykDanghyan <haykhermine07@gmail.com>
2023-04-08 08:55:36 +0400

    LeetCode #1979 problem

---
+++ homework2/leetcode1979.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int gcd(int a, int b) {
+        if (a == 0) {
+            return b;
+        }
+        return gcd(b % a, a);
+    }
+    int findGCD(vector<int>& nums) {
+        int max = *std::max_element(nums.begin(), nums.end());
+        int min = *std::min_element(nums.begin(), nums.end());
+        return gcd(max, min);
+    }
+};
